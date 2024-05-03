a384a26bb3c7aaf1f6514c71b92b7f5040011bd1
Mourtada <mourtadam21@gmail.com>
2024-05-03 03:58:50 +0200

    récuperation des données pour se connecter par des variables d'environnement au cas ou change de serveur de bd

--- www/connect.php
+++ www/connect.php
@@ -1,8 +1,18 @@
 <?php
+
+    // récuperation des données pour se connecter par des variables d'environnement 
+    $host = getenv('DB_HOST');
+    $username = getenv('DB_USER');
+    $password = getenv('DB_PASSWPORD');
+    $db = getenv('DB_NAME');
+
+    /*
     $host = "db";
     $username = "root";
     $password = "root";
     $db = "gestion_produits";
+    */
+
 
     $link = mysqli_connect($host,$username,$password,$db) or die ("Erreur de connexion à la base de données.");
     mysqli_set_charset($link,"utf8");
